87f6514fcbecb73a8347991792455f693f7554ea
Zakaria Faarah <zak.faarah@fridleymn.gov>
2023-10-08 20:49:18 -0500

    Still need to do some cleanup. Added pages

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+document.getElementById("one").addEventListener("click", function() {
+    // Redirect to login.html
+    window.location.href = "login.html";
+});
+
+document.getElementById("two").addEventListener("click", function() {
+    // Redirect to passgen.html
+    window.location.href = "passgen.html";
+});
