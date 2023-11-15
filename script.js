ae6f0d3bd41d6beb533a92a66490fbe08ba71bc3
Zakaria Faarah <zak.faarah@fridleymn.gov>
2023-11-15 13:20:31 -0600

    fixed

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// // script.js
-// window.onload = function() {
-// //     document.getElementById("one").addEventListener("click", function() {
-// //         // Redirect to login.html
-// //         window.location.href = "login.html";
-// //     });
+// script.js
+window.onload = function() {
+    document.getElementById("one").addEventListener("click", function() {
+        // Redirect to login.html
+        window.location.href = "login.html";
+    });
     
-//     document.getElementById("two").addEventListener("click", function() {
-//         // Redirect to passgen.html
-//         window.location.href = "passgen.html";
-//     });
+    document.getElementById("two").addEventListener("click", function() {
+        // Redirect to passgen.html
+        window.location.href = "passgen.html";
+    });
     
-// // };
+};
